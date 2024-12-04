3090b0e7798ce61951a9dd4c5ab957061191960f
Yusuf Oztoprak <147493350+YusufOztoprak@users.noreply.github.com>
2024-12-04 14:53:22 +0300

    UPDATE

---
+++ src/main/java/com/example/veriy/Cikolata.java
@@ -0,0 +1,24 @@
+package com.example.veriy;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Cikolata extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AnaEkran.fxml"));
+        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.setTitle("Ana Ekran");
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(800);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.veriy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.veriy to javafx.fxml;
-    exports com.example.veriy;
-}
